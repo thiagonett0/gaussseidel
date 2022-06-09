a69f3bff73e2fa3216be4201eb98eee6976ce7f1
thiagonett0 <alohaquant@protonmail.com>
2022-06-09 18:34:01 -0300

    v2

--- python/gaussseidel.py
+++ python/gaussseidel.py
@@ -4,12 +4,39 @@ Descrição: código que soluciona qualquer equação do tipo Ax=b, para qualque
 
 
 def gaussseidel(A, b, erro):
+
+    """
+    Descrição: calcula a solução aproximada x de um sistema Ax=b, para A simétrica e positiva definida. 
+            O algoritmo segue conforme o discutido em Teorema 10.2.1, página 512 de Matrix Computations, 
+            Golub e Van Loan, com algumas observações:
+
+                i) arbitra-se como estimativa inicial o vetor nulo;
+                ii) adota-se um critério de parada conservador, como segue no código;
+                iii) o funcionamento do código é análogo a qualquer processo iterativo, i.e.:
+
+                        iii.a) após estabelecidos a estimativa inicial, a tolerância e o critério de parada, calcula
+                                a solução no próximo passo por meio da expressão pré determinada;
+                        iii.b) verifica-se se a solução satisfaz o critério de parada. Se sim, altera a variável 
+                                booleana parada;
+                        iii.c) a variável que guardava o passo anterior é alterada de forma a guarda a solução no
+                                no passo atual;
+                        iii.d) repete até a variável parada ser alterada;
+
+    Entrada(s):
+                i) A (list): matriz simétrica e positiva definida;
+                ii) b (func): vetor independente do sistema;
+                iii) erro (float): tolerância de erro aceitável;
+    
+    Saída(s):
+                i) x (list): solução aproximada do sistema.
+    """
+
     x0 = [0]*len(A)
     x = [None]*len(A)
     parada = False
     while not parada:
         for i in range(len(A)):
-            x[i] = (b[i] - soma1(A, x, i) - soma2(A, x0, i, len(A)))/A[i][i]
+            x[i] = (b[i] - soma1(A, x, i) - soma2(A, x0, i))/A[i][i]
         if norma(subtracao(b, produtoMatrizVetor(A, x0))) < erro:
             parada = True
         x0 = x
@@ -18,15 +45,43 @@ def gaussseidel(A, b, erro):
 
 
 def soma1(A, x, i):
+
+    """
+    Descrição: calcula o primeiro somatório apresentado na última expressão do
+                arquivo readme.md;
+    
+    Entrada(s):
+                i) A (list): matriz do sistema;
+                ii) x (list): vetor iterando, no passo posterior;
+                iii) i (int): i-ésima entrada do vetor x;
+    
+    Saída(s):
+                i) soma (float): resultado do somatório.
+    """
+
     soma = 0
     for j in range(i):
         soma += A[i][j]*x[j]
     return soma
 
 
-def soma2(A, x, i, n):
+def soma2(A, x, i):
+
+    """
+    Descrição: calcula o segundo somatório apresentado na última expressão do
+                arquivo readme.md;
+    
+    Entrada(s):
+                i) A (list): matriz do sistema;
+                ii) x (list): vetor iterando, no passo anterior;
+                iii) i (int): i-ésima entrada do vetor x;
+    
+    Saída(s):
+                i) soma (float): resultado do somatório.
+    """
+
     soma = 0
-    for j in range(i+1, n):
+    for j in range(i+1, len(A)):
         soma += A[i][j]*x[j]
     return soma
 
@@ -107,7 +162,3 @@ def produtoMatrizVetor(A, v):
     for i in A:
         p.append(produtoescalar(i, v))
     return p
-
-A = [[4, -1, 1], [-1, 5, 2], [1, 2, 9]]  # Ser SPD!
-b = [2, 10, -1]
-gaussseidel(A, b, 1e-6)
